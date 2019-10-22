961f754d859c58c750481185162fb61eba39e71a
Adrien Coffre <ext-adrien.coffre@here.com>
2019-10-22 13:53:27 +0200

    commit -m

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import { MapView } from "@here/harp-mapview";
+import { GeoCoordinates } from "@here/harp-geoutils";
+import { APIFormat, OmvDataSource } from "@here/harp-omv-datasource";
+import { MapControls, MapControlsUI } from "@here/harp-map-controls";
+
+const map = new MapView({
+    canvas: document.getElementsByTagName("canvas")[0],
+    theme: "./resources/berlin_tilezen_base_globe.json"
+});
+window.addEventListener("resize", () => {
+    map.resize(window.innerWidth, window.innerHeight);
+});
+
+const omvDataSource = new OmvDataSource({
+    baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
+    concurrentDecoderScriptUrl: "dist/harp-worker.bundle.js",
+    apiFormat: APIFormat.XYZOMV,
+    styleSetName: "tilezen",
+    maxZoomLevel: 17,
+    authenticationCode: "AGln99HORnqL1kfIQtsQl70"
+});
+
+map.addDataSource(omvDataSource);
+
+const controls = new MapControls(map);
+const ui = new MapControlsUI(controls);
+map.canvas.parentElement!.appendChild(ui.domElement);
+
+const NY = new GeoCoordinates(40.71, -74.007);
+map.setCameraGeolocationAndZoom(NY, 3.2);
